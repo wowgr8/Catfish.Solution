c35916b37e4f23a27d73c5257a9ee4e3a2616ae5
Patrick Dolan <dolanp1992@gmail.com>
2022-04-24 11:26:15 -0700

    add basic models
    Co-authored-by: Ebru Rice <ebruerenb@gmail.com>
    Co-authored-by: Cesar Lopez <Lopez.cesar.aug@gmail.com>
    Co-authored0by: Jack Skelton <skelton.jt9@gmail.com>
    Co-authored-by: Marni Sucher <suchermarni@gmail.com>

--- CatFish/Models/ApplicationUser.cs
+++ CatFish/Models/ApplicationUser.cs
@@ -1,9 +1,33 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
-
+using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Collections.Generic;
 namespace CatFish.Models
 {
     public class ApplicationUser : IdentityUser
     {
-
+        public ApplicationUser()
+        {
+            this.Matches = new HashSet<Match>();
+        }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string Bio { get; set; }
+        public string About { get; set; }
+        [Required]
+        public int Age { get; set; }
+        [Required]
+        public string Species { get; set; }
+        [Required]
+        public string Breed { get; set; }
+        public string ImageName { get; set; }
+        [NotMapped]
+        public IFormFile ImageFile { get; set; }
+        public virtual ICollection<Match> Matches { get; set; }
     }
 }
--- CatFish/Models/Match.cs
+++ CatFish/Models/Match.cs
@@ -2,6 +2,10 @@ namespace CatFish.Models
 {
   public class Match
   {
-    
+    public int MatchId;
+
+    public string UserId;
+
+    public virtual ApplicationUser ApplicationUser { get; set; }
   }
 }
