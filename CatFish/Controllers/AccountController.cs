ea083ef2f37e57eec04a3c1f723f774e092944f4
Patrick Dolan <dolanp1992@gmail.com>
2022-04-25 19:18:10 -0700

    add browse page with all users

--- CatFish/Controllers/AccountController.cs
+++ CatFish/Controllers/AccountController.cs
@@ -32,6 +32,12 @@ namespace CatFish.Controllers
             return View(currentUser);
         }
 
+        public ActionResult Browse()
+        {
+            var users = _userManager.Users;
+            return View(users);
+        }
+
         public IActionResult Register()
         {
             return View();
