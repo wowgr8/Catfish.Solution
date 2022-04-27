ae6cce4cf88803d46d7c79f202ee80d749316fa5
Patrick Dolan <dolanp1992@gmail.com>
2022-04-26 19:18:03 -0700

    change property type of responses to int
    Co-authored-by: Ebru Rice <ebruerenb@gmail.com>
    Co-authored-by: Cesar Lopez <Lopez.cesar.aug@gmail.com>
    Co-authored0by: Jack Skelton <skelton.jt9@gmail.com>
    Co-authored-by: Marni Sucher <suchermarni@gmail.com>

--- CatFish/Models/Match.cs
+++ CatFish/Models/Match.cs
@@ -6,8 +6,8 @@ namespace CatFish.Models
 
     public string User1Id { get; set; }
     public string User2Id { get; set; }
-    public bool User1Response { get; set; }
-    public bool User2Response { get; set; }
+    public int User1Response { get; set; }
+    public int User2Response { get; set; }
 
     public virtual ApplicationUser ApplicationUser { get; set; }
   }
