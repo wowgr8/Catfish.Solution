2705d7970442e06e302c23c01c0e1179a022484f
Patrick Dolan <dolanp1992@gmail.com>
2022-04-26 19:17:34 -0700

    add migrations for matches
    Co-authored-by: Ebru Rice <ebruerenb@gmail.com>
    Co-authored-by: Cesar Lopez <Lopez.cesar.aug@gmail.com>
    Co-authored0by: Jack Skelton <skelton.jt9@gmail.com>
    Co-authored-by: Marni Sucher <suchermarni@gmail.com>

---
+++ CatFish/Migrations/20220426035216_ChangeReponse.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CatFish.Migrations
+{
+    public partial class ChangeReponse : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "User2Response",
+                table: "Matches",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldType: "tinyint(1)");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "User1Response",
+                table: "Matches",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldType: "tinyint(1)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "User2Response",
+                table: "Matches",
+                type: "tinyint(1)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "User1Response",
+                table: "Matches",
+                type: "tinyint(1)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+    }
+}
