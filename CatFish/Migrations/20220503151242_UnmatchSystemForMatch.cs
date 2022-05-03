005daf10c2f1a38f8aaaa634fb469c35d41bd812
Patrick Dolan <dolanp1992@gmail.com>
2022-05-03 09:05:13 -0700

    add unmatch system migrations

---
+++ CatFish/Migrations/20220503151242_UnmatchSystemForMatch.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CatFish.Migrations
+{
+    public partial class UnmatchSystemForMatch : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Unmatched",
+                table: "Matches",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Unmatched",
+                table: "Matches");
+        }
+    }
+}
